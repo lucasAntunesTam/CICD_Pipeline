9e0fd7964a8e4a6ff6b88fad7ee7a1c6035bed98
lucasAntunesTam <164916885+lucasAntunesTam@users.noreply.github.com>
2024-09-09 15:09:54 -0300

    Update AddToFolder.ps1

--- Scripts/AddToFolder.ps1
+++ Scripts/AddToFolder.ps1
@@ -9,37 +9,36 @@
   }
 
 function Get-ProcessInfo {
-    param ($Token, $OrgId)   
+    param ($Token)   
             
     $header = @{
         Authorization                 = ('Bearer {0}' -f $Token)
         Accept                        = 'application/json'
-        "X-UIPATH-OrganizationUnitId"   = $OrgId
     }
     $process = Invoke-RestMethod -Method GET -Uri "https://staging.uipath.com/lucas_antunes_tam/DefaultTenant/orchestrator_/odata/Processes?%24orderby=Published%20desc&%24top=1" -Headers $header -ContentType "application/json"
     return $process
 }
  
 function Add-Process {
-    param ($Token, $ProcessData, $OrgId)   
+    param ($Token, $ProcessData, $FolderId)   
             
       $header = @{
         Authorization                 = ('Bearer {0}' -f $Token)
         Accept                        = 'application/json'
-        "X-UIPATH-OrganizationUnitId"   = $OrgId
+        "X-UIPATH-OrganizationUnitId"   = $FolderId
     }
     
     $body = "{`"ProcessKey`": `"$($ProcessData.value.Title)`", `"ProcessVersion`": `"$($ProcessData.value.Version)`", `"Name`": `"$($ProcessData.value.Title)`", `"FeedId`": `"65c131da-6393-460c-af06-c70649b658ed`"}"
    
 try {
-    Invoke-RestMethod -Method POST -Uri "https://cloud.uipath.com/myriadgeneticsinc/dev/orchestrator_/odata/Releases" -Headers $header -body $body -ContentType "application/json"
+    Invoke-RestMethod -Method POST -Uri "https://staging.uipath.com/lucas_antunes_tam/DefaultTenant/orchestrator_/odata/Releases" -Headers $header -body $body -ContentType "application/json"
 }
    catch {
         Write-Host "Process exists"
     } 
 }
 
-$ProcessData = Get-ProcessInfo -Token ($token = Get-Token) -OrgId 508979
-Add-Process -Token ($token = Get-Token) -ProcessData $ProcessData -OrgId 508979
+$ProcessData = Get-ProcessInfo -Token ($token = Get-Token)
+Add-Process -Token ($token = Get-Token) -ProcessData $ProcessData -FolderId 1621959
 
 
